e7a0574deb669745e1069d40f1eb46e0c044b160
Marcus Kaa <mkaa2002@gmail.com>
2022-04-19 13:29:23 +0200

    custom error

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+import Error from '../public/img/stinky.jpg'
+
+function Custom404() {
+  return (
+    <div>
+        <div className='relative h-64 w-64'>
+            <Image src={Error} layout="fill"/>
+        </div>
+
+        <h1>Uh oh stinky poopy</h1>
+    </div>
+  )
+}
+
+export default Custom404
